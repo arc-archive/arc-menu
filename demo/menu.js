54a2dcc5aa5f16f1b5f21e61602042d487f8b3d0
Pawel Psztyc <jarrodek@gmail.com>
2021-02-13 19:33:58 -0800

    chore: updating dependencies
    Signed-off-by: Pawel Psztyc <jarrodek@gmail.com>

--- demo/menu.js
+++ demo/menu.js
@@ -41,7 +41,7 @@ class ComponentDemoPage extends DemoPage {
     ]);
     this.componentName = 'arc-menu';
     this.demoStates = ['Material', 'Anypoint'];
-    this.historyEnabled = false;
+    this.historyEnabled = true;
     this.compatibility = false;
     this.hideHistory = false;
     this.hideSaved = false;
